abf34b8b64f4bc223a69282aab9fdeefd7135e23
akash.verma2721245 <akash.verma@logictrixinfotech.com>
2024-04-21 11:49:12 +0530

    valid time update 24

--- client/src/pages/ApplyDoctor.js
+++ client/src/pages/ApplyDoctor.js
@@ -1,62 +1,133 @@
-import React from 'react'
-import Layout from "../components/Layout"
-import { Col, Form, Input, Row } from 'antd'
+import React from "react";
+import Layout from "../components/Layout";
+import { Col, Form, Input, Row, TimePicker } from "antd";
 
 function ApplyDoctor() {
-    const handleFinish = async(values)=>{
-        console.log(values);
+  const handleFinish = async (values) => {
+    try {
+      console.log("value..............", values);
+    } catch (err) {
+      console.log(err);
     }
+  };
   return (
     <Layout>
-        <h1 className='text-center'>Apply Doctor</h1>
-        <Form layout='vertical' onFinish={handleFinish} className='m-2'>
-        <h6 className='text-dark'>Personal Details:</h6>
-            <Row gutter={20}>
-                <Col xs={24} md={24} lg={8}>
-                    <Form.Item label="firstName" required>
-                        <Input type='text'  placeholder='Enter firstName'></Input>
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                <Form.Item label="lastName" required>
-                        <Input type='text'  placeholder='Enter lastName'></Input>
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                    <Form.Item label="email" required>
-                        <Input type='email'  placeholder='Enter email'></Input>
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                    <Form.Item label="phone" required>
-                        <Input type='text'  placeholder='Enter phone'></Input>
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                    <Form.Item label="specialization" required>
-                        <Input type='text'  placeholder='Enter specialization'></Input>
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                    <Form.Item label="experience" required>
-                        <Input type='text'  placeholder='Enter experience'></Input>
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                    <Form.Item label="feesPerCunsaltation" required>
-                        <Input type='text'  placeholder='Enter feesPerCunsaltation'></Input>
-                    </Form.Item>
-                </Col>
-                <Col xs={24} md={24} lg={8}>
-                    <Form.Item label="firstName" required>
-                        <Input type='text'  placeholder='Enter firstName'></Input>
-                    </Form.Item>
-                </Col>
-            </Row>
-        </Form>
+      <h1 className="text-center">Apply Doctor</h1>
+      <Form layout="vertical" onFinish={handleFinish} className="m-2">
+        <h6 className="text-dark">Personal Details:</h6>
+        <Row gutter={20}>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="FirstName"
+              name="firstName"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter firstName" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="LastName"
+              name="lastName"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter lastName" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="Email"
+              name="email"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="email" placeholder="Enter email" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="Phone"
+              name="phone"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter phone" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="Website"
+              name="website"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter website" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="Address"
+              name="address"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter address" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <h6 className="text-dark">Professional Details:</h6>
+        <Row gutter={20}>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="Specialization"
+              name="specialization"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter specialization" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="Experience"
+              name="experience"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter experience" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="FeesPerCunsaltation"
+              name="feesPerCunsaltation"
+              required
+              rules={[{ required: true }]}
+            >
+              <Input type="text" placeholder="Enter feesPerCunsaltation" />
+            </Form.Item>
+          </Col>
+          <Col xs={24} md={24} lg={8}>
+            <Form.Item
+              label="Timings"
+              name="timings"
+              required
+              rules={[{ required: true }]}
+            >
+              <TimePicker.RangePicker format="HH:MM" />
+            </Form.Item>
+          </Col>
+        </Row>
+        <div className="d-flex justify-content-center">
+          <button className="btn btn-primary" type="submit" >
+            Submit
+          </button>
+        </div>
+      </Form>
     </Layout>
-
-  )
+  );
 }
 
-export default ApplyDoctor
+export default ApplyDoctor;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,87 +1,106 @@
-const User = require("../models/user")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const helper = require("../utils/helper")
-const userRegister = async(req,res)=>{
-    try{
-        let emailMatch = await User.findOne({
-            email: req.body.email
-        });
-        const hashedPassword = await bcrypt.hash(req.body.password,10)
-        if (emailMatch) {
-            return res.status(200).json({
-                success : false,
-                message: "User Exist Please Login !"
-            });
-        }
-        const user = User({
-            name: req.body.name,
-            email:req.body.email,
-            password : hashedPassword
-        })
-        let response = await user.save();
-        delete response.password
-        return res.status(201).json({
-            success : true,
-            msg: "USER REGISTER SUCCESSFULLY.",
-            data: response
-        });
-    }catch(err){
-            return res.json(helper.showInternalServerErrorResponse("Internal server error"));
+const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const helper = require("../utils/helper");
+const userRegister = async (req, res) => {
+  try {
+    let emailMatch = await User.findOne({
+      email: req.body.email,
+    });
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    if (emailMatch) {
+      return res.status(200).json({
+        success: false,
+        message: "User Exist Please Login !",
+      });
     }
-}
+    const user = User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    let response = await user.save();
+    delete response.password;
+    return res.status(201).json({
+      success: true,
+      msg: "USER REGISTER SUCCESSFULLY.",
+      data: response,
+    });
+  } catch (err) {
+    return res.json(
+      helper.showInternalServerErrorResponse("Internal server error")
+    );
+  }
+};
 
 const login = async (req, res) => {
-    try {
-        let user = await User.findOne({
-            email: req.body.email
+  try {
+    let user = await User.findOne({
+      email: req.body.email,
+    });
+    if (user) {
+      let passwordMatch = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (passwordMatch) {
+        let token = jwt.sign(
+          {
+            _id: user._id.toString(),
+          },
+          process.env.secret_key,
+          {
+            expiresIn: "24h",
+          }
+        );
+        user.tokens.push({ token });
+        await user.save();
+        res.status(201).json({
+          success: true,
+          message: "Login Success",
+          data: user,
+          token,
         });
-        if (user) {
-            let passwordMatch = await bcrypt.compare(req.body.password, user.password);
-            if (passwordMatch) {
-                let token = jwt.sign({
-                    _id: user._id.toString()
-                }, process.env.secret_key, {
-                    expiresIn: "1h"
-                })
-                user.tokens.push({ token })
-                await user.save()
-                res.status(201).json({
-                    success : true,
-                    message: "Login Success",
-                    data: user,
-                    token
-
-                });
-            } else {
-                return res.json({success : false,message:"Invalid Email or Password !"});
-            };
-        } else {
-            return res.json({success : false,message:"Invalid Email or Password !"})  ;
-        };
-    } catch (err) {
-        console.log(err);
-        return res.json(helper.showInternalServerErrorResponse("Internal server error"));
-
-    };
-}
-
-const getUser = async(req,res)=>{
-    try{
-        const userData = await User.findOne({_id:req.user._id})
-        delete userData.password
-        if(!userData){
-            return res.status(200).send({message:'User not Found', success:false})
-        }
-        return res.status(200).send({success:true, data:userData})
-    }catch(err){
-        console.log(err);
-        return res.json(helper.showInternalServerErrorResponse("Internal server error"));
+      } else {
+        return res.json({
+          success: false,
+          message: "Invalid Email or Password !",
+        });
+      }
+    } else {
+      return res.json({
+        success: false,
+        message: "Invalid Email or Password !",
+      });
     }
-}
+  } catch (err) {
+    console.log(err);
+    return res.json(
+      helper.showInternalServerErrorResponse("Internal server error")
+    );
+  }
+};
+
+const getUser = async (req, res) => {
+  try {
+    const userData = await User.findOne({ _id: req.user._id });
+    delete userData.password;
+    if (!userData) {
+      return res
+        .status(200)
+        .send({ message: "User not Found", success: false });
+    }
+    return res.status(200).send({ success: true, data: userData });
+  } catch (err) {
+    console.log(err);
+    return res.json(
+      helper.showInternalServerErrorResponse("Internal server error")
+    );
+  }
+};
 
 module.exports = {
-    userRegister,
-    login,
-    getUser
-}
+  userRegister,
+  login,
+  getUser,
+};
